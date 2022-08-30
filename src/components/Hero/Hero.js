c61f71fb3681e520339b64d58b6819421693f622
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-30 15:10:25 -0700

    Set up the page container

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const heroPage = {
+    height: "100%",
+    background: "blue",
+}
+
+const Hero = () => {
+    return (
+        <div>
+            Here is the Hero Container
+        </div>
+    );
+}
+
+export default Hero;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            Here is the Nav Bar
+        </div>
+    );
+}
+
+export default Nav;
