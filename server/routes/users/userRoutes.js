7ef0c207a24ad24de77ec32ec1ec345a9c27f294
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-09-15 20:33:44 -0700

    Fixed import

--- server/routes/users/userRoutes.js
+++ server/routes/users/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, loginUser, getMe } = require('../controllers/userControllers.js')
+const { registerUser, loginUser, getMe } = require('../../controllers/userController.js')
 
 
 router.get('/', registerUser)
