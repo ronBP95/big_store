e1a9aedb1fcf8606e7c7cc449d70fe1cfdce41c1
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-09-15 20:04:20 -0700

    Added backend necessary dependencies

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send("hi")
 })
 
+app.get('/users', (req, res) => {
+    
+})
+
 app.listen(PORT, () => {
     console.log(`The server is listening on port: ${PORT}`)
 })
