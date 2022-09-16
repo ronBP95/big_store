f2fc6b6ba2c22f8a055b4c696b30d4b878eb4552
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-09-15 20:44:30 -0700

    Resolved mongo URI must be a string issue

--- server/routes/users/userRoutes.js
+++ server/routes/users/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const { registerUser, loginUser, getMe } = require('../../controllers/userController.js')
 
-
 router.get('/', registerUser)
 router.post('/', loginUser)
 router.get('/me', getMe)
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
-const express = require('express')
+const express = require('express');
+const { connect } = require('mongoose');
+require('dotenv').config()
 const app = express();
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 4000;
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
