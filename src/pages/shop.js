09d51bc3c4904290901b53b1e82b17e5c25dfdba
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-09-22 12:11:46 -0700

    Built out basic shop grid

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,11 +1,16 @@
-import { Container} from '@mui/material';
+import { Grid, Container } from '@mui/material';
 import React from 'react';
 import ShopCard from '../components/ShopCard/ShopCard'
 
 const Shop = () => {
     return (
-        <Container sx={{ width: 900, marginY: 5}}>
-            Hi
+        <Container sx={{marginY: 5, width: 900}}>
+            <Grid container spacing={5}>
+                <ShopCard />
+                <ShopCard />
+                <ShopCard />
+                <ShopCard />
+            </Grid>
         </Container>
     );
 }
