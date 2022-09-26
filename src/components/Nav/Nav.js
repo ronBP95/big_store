932a337d9963682e1dbd8b316a8a08bf35d61576
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-09-26 14:48:16 -0700

    Cart modal front end built out

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,14 +5,20 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import Modal from '@mui/material/Modal';
 import { Link } from "gatsby"
+
+// Material-UI Icon Imports
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 // Routes
 const Products = <Button sx={{color: 'white'}} href='/shop'>products</Button>
@@ -40,6 +46,33 @@ const Nav = () => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  // Modal Style
+  const style = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    top: "50%",
+    left: '87.45%',
+    transform: 'translate(-50%, -50%)',
+    paddingTop: "10px",
+    width: "25vw",
+    height: "99vh",
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+  };
+
+  const checkoutBar = {
+    paddingBottom: '10px'
+  }
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
 
   return (
     <AppBar position="static" sx={{backgroundColor: "#444444"}}>
@@ -130,35 +163,33 @@ const Nav = () => {
               </Button>
             ))}
           </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+          <Box>
+            <AccountCircleIcon />
+            <ShoppingCartIcon onClick={handleOpen} sx={{marginLeft: 2, cursor: 'pointer'}}/>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+              >
+              <Box sx={style}>
+                <Box sx={{textAlign: 'center', width: '100%'}}>
+                  <Box sx={{display: 'flex', justifyContent: 'end', width: '100%'}}>
+                    <CloseIcon sx={{cursor: 'pointer', padding: 0.5}} onClick={handleClose}/>
+                  </Box>
+                  <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Your Cart
+                  </Typography>
+                  <Typography sx={{ mt: 2, paddingLeft: 1, paddingRight: 1}} id="modal-modal-description">
+                    Your cart is empty. Fill it with something good.
+                  </Typography>
+                  <Button href='/shop'>Shop All</Button>
+                  </Box>
+                <Box sx={checkoutBar}>
+                  Checkout, HR, Subtotal, Shipping
+                </Box>
+              </Box>
+            </Modal>
           </Box>
         </Toolbar>
       </Container>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Nav } from './Nav/Nav'
 export { default as Hero } from './Hero/Hero'
 export { default as Footer } from './Footer/Footer'
-export { default as ShopNav } from './ShopNav/ShopNav'
 export { default as ShopCard } from './ShopCard/ShopCard'
