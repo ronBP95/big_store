d01e68e7e712e0a083e799d5046a5269e51b835a
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-09-28 12:06:20 -0700

    Profile Skeleton built out. Working on axios request

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Nav from '../components/Nav/Nav'
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 const Profile = () => {
     return (
         <div>
-            Profile Page
+            <Nav />
+            <Box sx={{display: "flex", flexDirection:"column", justifyContent: "center", alignItems: "center", marginTop: 5}}>
+                <Box>
+                    Profile:
+                </Box>
+                <Box>
+                    Name:
+                </Box>
+                <Box>
+                    Email:
+                </Box>
+                <Box>
+                    Order History:
+                </Box>
+            </Box>
         </div>
     );
 }
