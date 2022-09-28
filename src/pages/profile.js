f417aa7316032491364a0c81b0ea49f26f5c358e
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-09-28 13:03:10 -0700

    The me route was hit but need to add the data on the backend

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import * as React from 'react';
 import Nav from '../components/Nav/Nav'
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
+import axios from 'axios'
+
+// Mui Imports
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Container from '@mui/material/Container'
 
 const Profile = () => {
+
+    let token = localStorage.getItem('token')
+
+    const [name, setName] = React.useState('')
+    const [email, setEmail] = React.useState('')
+
+    const getMe = async () => {
+        await axios({
+            method: "get",
+            url: "http://localhost:4000/api/users/me",
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+        })
+        .then(function (res) {
+            console.log(res)
+            setName(res.data.name)
+        })
+    }
+
+    React.useEffect(() => {
+        getMe();
+    }, []);
+
+    console.log(name)
+
     return (
         <div>
             <Nav />
-            <Box sx={{display: "flex", flexDirection:"column", justifyContent: "center", alignItems: "center", marginTop: 5}}>
-                <Box>
-                    Profile:
-                </Box>
-                <Box>
-                    Name:
-                </Box>
-                <Box>
-                    Email:
-                </Box>
-                <Box>
-                    Order History:
-                </Box>
-            </Box>
+            <Container sx={{width: 750}}>
+                <Paper sx={{width: "100%", height: 150, marginTop: 5, padding: 1}}>
+                    <Box>
+                        Profile
+                    </Box>
+                    <Box>
+                        Name: {name}
+                    </Box>
+                    <Box>
+                        Email
+                    </Box>
+                </Paper>
+                <Paper sx={{width: "100%", height: 150, marginTop: 5, padding: 1}}>
+                    Order History
+                </Paper>
+            </Container>
         </div>
     );
 }
