5d61967093157c9537a3ea2f22838b439e141998
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-10-05 21:07:43 -0700

    Created basic Grid for grid page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ import { ToastContainer } from "react-toastify"
 import Box from '@mui/material/Box';
 import Typography from "@mui/material/Typography";
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles';
 
 // Icon Imports
 import SouthIcon from '@mui/icons-material/South';
@@ -16,20 +19,31 @@ import { HomeNav } from '../components/index'
 // Image Imports
 import storefront from '../images/storefront.jpg'
 
+// Grid Item
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  height: "50%"
+}));
+
 const IndexPage = () => {
   return (
     <Box className="landing">
       <Box className="nav">
         <HomeNav sx={{ height: "7.4%", minHeight: 60 }} />
       </Box>
+      
+      {/* Hero Container */}
       <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "75%", backgroundImage: `url(${storefront})`, backgroundSize: "cover"}}>
         <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-between", height: "100%"}}>
-          <Box></Box>
+          <Box alt="gutter"></Box>
           <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
             <Typography variant="h4" sx={{color: "white", fontSize: "27pt"}}>UPGRADE YOUR SHOPPING</Typography>
             <Box sx={{marginTop: 10, textAlign: "center"}}>
               <Typography sx={{color: "white"}}>Enjoy new products available only to members</Typography>
-              <Button variant="contained" sx={{marginTop: 3, marginBottom: 2, background: "#ff3366", height: "70px", width: "200px", "&.MuiButtonBase-root:hover": {
+              <Button href="/register" variant="contained" sx={{marginTop: 3, marginBottom: 2, background: "#ff3366", height: "70px", width: "200px", "&.MuiButtonBase-root:hover": {
                 bgcolor: "red"
               }}}>Register</Button>
               <Typography sx={{color: "white"}}>Discover the experience</Typography>
@@ -38,6 +52,22 @@ const IndexPage = () => {
           <SouthIcon sx={{color: "white", paddingBottom: 3}} />
         </Box>
       </Box>
+      {/* Hero Container */}
+      
+      {/* Grid Container */}
+      <Grid container spacing={2} sx={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", width: "100%", height: "100vh"}}>
+        <Grid item xs={4}>
+            <Item>Step 1</Item>
+        </Grid>
+        <Grid item xs={4}>
+            <Item>Step 2</Item>
+        </Grid>
+        <Grid item xs={4}>
+            <Item>Step 3</Item>
+        </Grid>
+      </Grid>
+      {/* Grid Container */}
+
       <Box className="homePage">
         <Box className="landingImg"></Box>
         <Box className="landingTxt">
