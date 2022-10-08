1b6c27d265428a921feb4f2ac4cefca5e3caf516
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-10-08 15:57:25 -0700

    Updated profile grab route to include orderHistory property

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -72,13 +72,14 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 const getMe = asyncHandler(async (req, res) => {
-    const { _id, name, email, cart } = await User.findById(req.user.id)
+    const { _id, name, email, cart, orderHistory } = await User.findById(req.user.id)
     console.log( _id, name, email )
     res.status(200).json({
         id: _id,
         name,
         email,
         cart,
+        orderHistory,
         message: "Profile successfully grabbed",
     })
 
