7410ebf8f83adc8bc66cc4091152bb641ee0e944
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-10-12 00:20:29 -0700

    Responsiveness complete on the hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,14 +14,14 @@ import '../../pages/index.css'
 
 const Hero = () => {
     return (
-    <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "85%", backgroundImage: `url(${storefront})`, backgroundSize: "cover"}}>
+    <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "85%", minHeight: "600px", backgroundImage: `url(${storefront})`, backgroundSize: "cover"}}>
         <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-between", height: "100%"}}>
             <Box alt="gutter"></Box>
             <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                 <Typography variant="h4" sx={{color: "white", fontSize: "27pt", fontFamily: "Inter", textAlign: "center"}}>UPGRADE YOUR SHOPPING</Typography>
                 <Box sx={{width: "35%", marginTop: 2, borderBottom: "3px solid #ff3366"}}></Box>
                 <Box sx={{marginTop: 10, textAlign: "center"}}>
-                    <Typography sx={{color: "white", fontFamily: "Inter"}}>Enjoy new products available only to memers</Typography>
+                    <Typography sx={{color: "white", fontFamily: "Inter"}}>Enjoy new products available only to members</Typography>
                     <Button href="/register" variant="contained" sx={{marginTop: 3, marginBottom: 2, background: "#ff3366", height: "50px", width: "150px", "&.MuiButtonBase-root:hover": {
                     bgcolor: "red"
                     }}}>Register</Button>
